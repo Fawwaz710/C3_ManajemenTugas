69ced992f42a82537af6cba992afd96a573aaa47
FAWWAZ <fawwaznaufaldzulfikar@gmail.com>
2026-04-15 21:19:44 +0700

    Membuat Class Koneksi

---
+++ C3_ManajemenTugas/Koneksi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace C3_ManajemenTugas
+{
+    internal class Koneksi
+    {
+        public SqlConnection GetConn()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=KAIDEN\\BLAZE;Initial Catalog=TugasDB;Integrated Security=True";
+
+            return conn;
+        }
+    }
+}

0e577aed1de9fadbc2c13109aeabfa3622e46eeb
FAWWAZ <fawwaznaufaldzulfikar@gmail.com>
2026-04-15 21:42:39 +0700

    Menambahkan DataGridView

--- C3_ManajemenTugas/Form1.Designer.cs
+++ C3_ManajemenTugas/Form1.Designer.cs
@@ -28,21 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvTugas = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTugas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvTugas
+            // 
+            this.dgvTugas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTugas.Location = new System.Drawing.Point(129, 288);
+            this.dgvTugas.Name = "dgvTugas";
+            this.dgvTugas.RowHeadersWidth = 51;
+            this.dgvTugas.RowTemplate.Height = 24;
+            this.dgvTugas.Size = new System.Drawing.Size(495, 150);
+            this.dgvTugas.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(129, 57);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.Size = new System.Drawing.Size(8, 8);
+            this.dataGridView2.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dgvTugas);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTugas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvTugas;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
 

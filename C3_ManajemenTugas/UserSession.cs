0ab08f184576b4b04508f871ba0ae83cd1f43df1
FAWWAZ <fawwaznaufaldzulfikar@gmail.com>
2026-05-13 23:23:10 +0700

    Membuat usersession

---
+++ C3_ManajemenTugas/UserSession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3_ManajemenTugas
+{
+    public static class UserSession
+    {
+        public static int UserId;
+        public static string Nama;
+        public static string Role;
+    }
+}
